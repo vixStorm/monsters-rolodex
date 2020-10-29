88b38109824e8719eba2245f2a0ab70dec969356
vixStorm <vkrnt.lal@rediffmail.com>
2020-10-29 19:42:40 +0530

    added size of search box

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 import './search-box.style.css';
 
 export const SearchBox = (props) => {
+    const inputBoxSize = {
+        height: "40px",
+        borderRadius: "6px"
+    }
     return(
         <React.Fragment>
             <input 
+            style={inputBoxSize}
             type="search" 
             placeholder={props.placeholder} 
             onChange={props.handleChange}/>
