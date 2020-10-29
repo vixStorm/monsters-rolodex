49878058c3054f2d125c39f049041bd7f25467f0
vixStorm <vkrnt.lal@rediffmail.com>
2020-10-29 19:26:24 +0530

    Updates

---
+++ precache-manifest.139a5ff4255229b8194b887d06681083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8451e7e445fe93724a3bed7aceb010b4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2d64ee560a590db7f15e",
+    "url": "/monsters-rolodex/static/css/main.ecd0ab1a.chunk.css"
+  },
+  {
+    "revision": "8416c93b1ffb842810de",
+    "url": "/monsters-rolodex/static/js/2.f28be3e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.f28be3e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d64ee560a590db7f15e",
+    "url": "/monsters-rolodex/static/js/main.0b2cb0e5.chunk.js"
+  },
+  {
+    "revision": "2060dba193321f92dfa3",
+    "url": "/monsters-rolodex/static/js/runtime-main.1db14954.js"
+  }
+]);
